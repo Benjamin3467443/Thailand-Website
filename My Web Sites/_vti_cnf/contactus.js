809dca5617fffbaa9f1f3d91917ca41ec06fe78d
Benjamin3467443 <151598359+Benjamin3467443@users.noreply.github.com>
2023-11-24 09:02:44 +0530

    Add files via upload

---
+++ My Web Sites/_vti_cnf/contactus.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|คนที่หล่อที่สุด\\Dell
+vti_modifiedby:SR|คนที่หล่อที่สุด\\Dell
+vti_timelastmodified:TW|23 Nov 2023 06:14:46 -0000
+vti_timecreated:TR|23 Nov 2023 06:13:48 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|cotactus.html
+vti_nexttolasttimemodified:TW|23 Nov 2023 06:14:46 -0000
+vti_cacheddtm:TX|23 Nov 2023 06:14:46 -0000
+vti_filesize:IR|400
---
+++ My Web Sites/_vti_cnf/result.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|คนที่หล่อที่สุด\\Dell
+vti_modifiedby:SR|คนที่หล่อที่สุด\\Dell
+vti_timelastmodified:TR|24 Nov 2023 03:11:22 -0000
+vti_timecreated:TR|24 Nov 2023 03:10:26 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|result.html
+vti_nexttolasttimemodified:TW|24 Nov 2023 03:10:26 -0000
+vti_cacheddtm:TX|24 Nov 2023 03:11:22 -0000
+vti_filesize:IR|622
